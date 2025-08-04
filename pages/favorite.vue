7d8244b5622fab848748dfc5c5259bf12e9b8559
ribbonfront <developer_15@wrfront2.com>
2025-08-04 17:39:26 +0700

    edited movies on fav&live pages

--- pages/favorite.vue
+++ pages/favorite.vue
@@ -12,16 +12,16 @@
                     </div>
                     <div class="my-3">
                         <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-7 gap-4">
-                            <div v-for="n in 40" :key="n">
+                            <div v-for="(each, n) in movList" :key="n">
                                 <div class="h-[90px] bg-gray-800 rounded-xl overflow-hidden">
                                     <img
-                                    :src="`https://picsum.photos/id/${n + 10}/200/300`"
+                                    :src="each.coverImg"
                                     class="w-full h-full object-cover"
                                     alt=""
                                     />
                                 </div>
                                 <div class="flex flex-col mt-2">
-                                    <span class="text-md text-white">Movie title</span>
+                                    <span class="text-md text-white">{{each.name}}</span>
                                 </div>
                                 <div class="flex">
                                     <div v-for="s in 3" :key="s">
@@ -47,6 +47,57 @@
 </template>
 
 <script setup lang="ts">
+type Movie = {
+  id: number;
+  name: string;
+  detail: string;
+  coverImg: string;
+  catId: number;
+  duration: number;
+};
+
+const movList = ref<Movie[]>([
+  {
+    id: 1,
+    name: "Wendy",
+    detail: "detail1",
+    coverImg: "/movies/mov1.png",
+    catId: 1,
+    duration: 2
+  },
+  {
+    id: 2,
+    name: "Interstellar",
+    detail: "detail2",
+    coverImg: "/movies/mov2.png",
+    catId: 2,
+    duration: 1
+  },
+  {
+    id: 3,
+    name: "After",
+    detail: "detail3",
+    coverImg: "/movies/mov3.png",
+    catId: 2,
+    duration: 3
+  },
+  {
+    id: 4,
+    name: "Joker",
+    detail: "detail4",
+    coverImg: "/movies/mov4.png",
+    catId: 1,
+    duration: 2
+  },
+  {
+    id: 5,
+    name: "After",
+    detail: "detail5",
+    coverImg: "/movies/mov5.png",
+    catId: 2,
+    duration: 1
+  },
+]);
 </script>
 
 <style scoped>
--- pages/livenow.vue
+++ pages/livenow.vue
@@ -12,16 +12,16 @@
                     </div>
                     <div class="my-3">
                         <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-                            <div v-for="n in 20" :key="n">
+                            <div v-for="(each, n) in movList" :key="n">
                                 <div class="h-[110px] bg-gray-800 rounded-xl overflow-hidden">
                                     <img
-                                    :src="`https://picsum.photos/id/${n + 10}/200/300`"
+                                    :src="each.coverImg"
                                     class="w-full h-full object-cover"
                                     alt=""
                                     />
                                 </div>
                                 <div class="flex flex-col my-2">
-                                    <span class="text-md text-white">Movie title</span>
+                                    <span class="text-md text-white">{{ each.name }}</span>
                                 </div>
                             </div>
                         </div>
@@ -36,6 +36,57 @@
 </template>
 
 <script setup lang="ts">
+type Movie = {
+  id: number;
+  name: string;
+  detail: string;
+  coverImg: string;
+  catId: number;
+  duration: number;
+};
+
+const movList = ref<Movie[]>([
+  {
+    id: 1,
+    name: "Wendy",
+    detail: "detail1",
+    coverImg: "/movies/mov1.png",
+    catId: 1,
+    duration: 2
+  },
+  {
+    id: 2,
+    name: "Interstellar",
+    detail: "detail2",
+    coverImg: "/movies/mov2.png",
+    catId: 2,
+    duration: 1
+  },
+  {
+    id: 3,
+    name: "After",
+    detail: "detail3",
+    coverImg: "/movies/mov3.png",
+    catId: 2,
+    duration: 3
+  },
+  {
+    id: 4,
+    name: "Joker",
+    detail: "detail4",
+    coverImg: "/movies/mov4.png",
+    catId: 1,
+    duration: 2
+  },
+  {
+    id: 5,
+    name: "After",
+    detail: "detail5",
+    coverImg: "/movies/mov5.png",
+    catId: 2,
+    duration: 1
+  },
+]);
 </script>
 
 <style scoped>
